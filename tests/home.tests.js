2b5f16b3972520786ca46260b968377b1a832eed
svetlana vasileva <svetlana.vasileva@afd.co.uk>
2024-03-26 16:41:18 +0000

    repo init

---
+++ tests/home.tests.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const fetch = require('node-fetch');
+
+suite('Home page', function() {
+  test('Page title', async function() {
+    let res = await fetch("http://localhost:8888/");
+    let body = await res.text();
+    assert.ok(body.includes("<h1>Welcome to Students Registry App</h1>"));
+  });
+  
+  test('Students count', async function() {
+    let res = await fetch("http://localhost:8888/");
+    let body = await res.text();
+    assert.ok(body.includes("Registered students: <b>2</b>"));
+  });
+});
